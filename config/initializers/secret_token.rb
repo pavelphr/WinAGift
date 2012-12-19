75d5bd2f32a34892676438835dd4a83599a191d5
Pavel Phren <pavelphr@gmail.com>
2012-12-19 23:41:24 +0200

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WinAGift::Application.config.secret_token = '7f6acc159c3324e68da4ea97770861b39350624a25011536f80d40fabbee5614152caf0524224284fb47bf5da7e4319b35e429dc2b91aea9d855ec6426305d39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WinAGift::Application.config.session_store :cookie_store, key: '_win_a_gift_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WinAGift::Application.config.session_store :active_record_store
