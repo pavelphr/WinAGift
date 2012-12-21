f01fec15c868c5d10abfa4b8c71f66dcd5188dac
Pavel Phren <pavelphr@gmail.com>
2012-12-21 22:49:58 +0200

    With pics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def logo
+		image_tag("logo_gift_s.png", :alt => "SeeNWin", :class => "round")
+	end
+
+	def title
+		base_title ="Win A Gift"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
 end
