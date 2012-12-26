82cfd15c6f10d7b22399e0f9292159b82b596013
Pavel Phren <pavelphr@gmail.com>
2012-12-26 23:48:05 +0200

    Added cookies use

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,27 @@
 class GamesController < ApplicationController
   def choose
+  	@title = "Choose"
+
+    if cookies[:alias].nil?
+      new_alias = [('a'..'z').to_a.shuffle.first, 
+        [('a'..'z').to_a.shuffle[0..3], 
+          (0..9).to_a.shuffle[0..1]].shuffle].join
+      @welcome = "Hello new user. Your alias will be " + new_alias
+      cookies[:alias] = new_alias
+    else
+      @welcome = "Hi, " + cookies[:alias] + ". Good to see you again."
+    end
   end
 
   def play
+  	@title = "Play"
   end
 
   def win
+  	@title = "Win"
   end
 
   def share
+  	@title = "Share"
   end
 end
